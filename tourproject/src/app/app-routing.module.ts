6eaf8ae65bcadd4762d4959dd753bfa638b547fb
Saksham Mittal <sakshammittal534@gmail.com>
2020-06-23 12:03:35 +0530

    initial commit

--- tourproject/src/app/app-routing.module.ts
+++ tourproject/src/app/app-routing.module.ts
@@ -5,17 +5,37 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { LoginComponent } from './login/login.component';
 import { dashboardComponent } from './dashboard/dashboard.component';
-import {dashheaderComponent} from './dashheader/dashheader.component'
+import {dashheaderComponent} from './dashheader/dashheader.component';
 import { AuthGuard } from './auth.guard';
+import {ContactComponent} from './contact/contact.component';
+import { TajmahalComponent } from './tajmahal/tajmahal.component';
+import { BanarasComponent } from './banaras/banaras.component';
+import { QutubComponent } from './qutub/qutub.component';
+import { IndiagateComponent } from './indiagate/indiagate.component';
+import { GoaComponent } from './goa/goa.component';
+import { GtempleComponent } from './gtemple/gtemple.component';
+import { InnerAboutComponent } from './inner-about/inner-about.component';
+
 
 
 const routes: Routes = [
   {path:'', component:WebpageComponent , children:[
 
     {path:'home', component:HomeComponent},
-    {path:'about', component:AboutComponent},
-    {path:'login', component:LoginComponent}
+   
+    {path:'contact', component:ContactComponent},
+    {path:'login', component:LoginComponent},
+    {path:'tajmahal', component:TajmahalComponent},
+    {path:'banaras' , component:BanarasComponent},
+    {path:'qutub', component:QutubComponent},
+    {path:'indiagate', component:IndiagateComponent},
+    {path:'goa', component:GoaComponent},
+    {path:'gtemple', component:GtempleComponent}
+   
   ]},
+  {path:'about', component:AboutComponent},
+  
+
   {path:'dashboard', component:dashboardComponent, canActivate:[AuthGuard]}
 
 ];
